479648f0770020b28d72b6f348668a61022eacf0
esrra66 <israbencheikh75@gmail.com>
2024-12-07 01:42:02 +0100

    js femme done

---
+++ page accuile/femme.js
@@ -0,0 +1,131 @@
+function verif(){
+    let b1 = document.getElementById("b1").checked;
+    let b2 = document.getElementById("b2").checked;
+    let b3 = document.getElementById("b3").checked;
+    let b4 = document.getElementById("b4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }
+
+}
+function verif1(){
+    let b1 = document.getElementById("bt1").checked;
+    let b2 = document.getElementById("bt2").checked;
+    let b3 = document.getElementById("bt3").checked;
+    let b4 = document.getElementById("bt4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }}
+function verif2(){
+        let b1 = document.getElementById("btn1").checked;
+        let b2 = document.getElementById("btn2").checked;
+        let b3 = document.getElementById("btn3").checked;
+        let b4 = document.getElementById("btn4").checked;
+        if (!b1 && !b2 && !b3 && !b4) {
+            alert("Vous choisir votre taille.");
+            return false;
+        }    
+
+}
+function verif3(){
+    let b1 = document.getElementById("btns1").checked;
+    let b2 = document.getElementById("btns2").checked;
+    let b3 = document.getElementById("btns3").checked;
+    let b4 = document.getElementById("btns4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+function verif4(){
+    let b1 = document.getElementById("btnn1").checked;
+    let b2 = document.getElementById("btnn2").checked;
+    let b3 = document.getElementById("btnn3").checked;
+    let b4 = document.getElementById("btnn4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+function verif5(){
+    let b1 = document.getElementById("btb1").checked;
+    let b2 = document.getElementById("btb2").checked;
+    let b3 = document.getElementById("btb3").checked;
+    let b4 = document.getElementById("btb4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+function verif6(){
+    let b1 = document.getElementById("bto1").checked;
+    let b2 = document.getElementById("bto2").checked;
+    let b3 = document.getElementById("bto3").checked;
+    let b4 = document.getElementById("bto4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+
+
+function verif7(){
+    let b1 = document.getElementById("btt1").checked;
+    let b2 = document.getElementById("btt2").checked;
+    let b3 = document.getElementById("btt3").checked;
+    let b4 = document.getElementById("btt4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+function verif8(){
+    let b1 = document.getElementById("btp1").checked;
+    let b2 = document.getElementById("btp2").checked;
+    let b3 = document.getElementById("btp3").checked;
+    let b4 = document.getElementById("btp4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+}
+function verif9(){
+    let b1 = document.getElementById("bttn1").checked;
+    let b2 = document.getElementById("bttn2").checked;
+    let b3 = document.getElementById("bttn3").checked;
+    let b4 = document.getElementById("bttn4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+function verif10(){
+    let b1 = document.getElementById("bts1").checked;
+    let b2 = document.getElementById("bts2").checked;
+    let b3 = document.getElementById("bts3").checked;
+    let b4 = document.getElementById("bts4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
+function verif11(){
+    let b1 = document.getElementById("btx1").checked;
+    let b2 = document.getElementById("btx2").checked;
+    let b3 = document.getElementById("btx3").checked;
+    let b4 = document.getElementById("btx4").checked;
+    if (!b1 && !b2 && !b3 && !b4) {
+        alert("Vous choisir votre taille.");
+        return false;
+    }    
+
+}
